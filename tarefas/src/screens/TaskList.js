5477c6f28aa738fe28a82b91e77d49c21c067858
Eduardo Cintra dos Santos <38438615+eduardocintrasantos@users.noreply.github.com>
2020-08-07 10:04:28 -0300

    toggleTask
    Implementação do filtro responsavel por ocultar as tarefas finalizadas.

--- tarefas/src/screens/TaskList.js
+++ tarefas/src/screens/TaskList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, StyleSheet, FlatList } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native';
 
 import commonStyles from '../commonStyles';
 import Task from '../components/Task';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import moment from 'moment';
 import 'moment/locale/pt-br';
@@ -11,6 +13,8 @@ import todayImage from '../../assets/imgs/today.jpg';
 
 export default class TaskList extends Component {
     state = {
+        showDoneTasks: true,
+        visibleTasks: [],
         tasks: [{
             id: Math.random(),
             desc: 'Comprar livro',
@@ -24,20 +28,57 @@ export default class TaskList extends Component {
         }]
     }
 
+    componentDidMount = () => {
+        this.filterTasks()
+    }
+
+    toggleFilter = () => {
+        this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null
+        if(this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+
+        this.setState({ visibleTasks })
+    }
+
+    toggleTask = taskId => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if(task.id === taskId) {
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+        })
+
+        this.setState({ tasks : tasks }, this.filterTasks)
+    }
+
     render() {
         const today = moment().format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
                 <ImageBackground source={todayImage} style={styles.backgound}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={this.toggleFilter}>
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                                size={20} color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subTitle}>{today}</Text>
                     </View>
                 </ImageBackground>
                 <View style={styles.taskContainer}>
-                    <FlatList data={this.state.tasks}
+                    <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
-                        renderItem={({item}) => <Task {...item} />}></FlatList>
+                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} />}></FlatList>
                 </View>
                 
             </View>
@@ -72,5 +113,11 @@ const styles = StyleSheet.create({
         color: commonStyles.colors.secondary,
         marginLeft: 20,
         marginBottom: 40,
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: Platform.OS === 'ios' ? 40 : 10
     }
 })
